4945922c232db97bf9634f38f6c7c7f90bbccd37
srithars <64460687+srithars@users.noreply.github.com>
2025-07-08 05:27:37 +0530

    Update Service.java

--- src/main/java/com/klu/Service.java
+++ src/main/java/com/klu/Service.java
@@ -28,6 +28,7 @@ public class Service {
 	}
 
 	
+	/*
 	public String deleteData(int id) {
 		if (r1.existsById(id)) {
 			r1.deleteById(id);
@@ -36,5 +37,6 @@ public class Service {
 			return "Product not found";
 		}
 	}
+	*/
 	
 }
